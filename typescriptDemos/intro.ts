3b4559ed3b25bd3e5702fb4386c0bffb7a4c0a42
Furkan Cemal Çalışkan <furkanc.caliskan@icloud.com>
2023-09-15 14:12:05 +0300

    added typescript demos

---
+++ typescriptDemos/intro.ts
@@ -0,0 +1,36 @@
+function hello(name:string){
+    return "Hello " + name
+}
+
+let message = hello('furkan')
+
+console.log(message)
+let sayi:number
+sayi = 10
+sayi = 10.4
+let sehir:string
+sehir = "Ankara"
+sehir = 'İstanbul'
+let isValid:boolean
+isValid = true
+
+let sayilar:number[] = [1,2,3]
+
+let sayilar2:Array<number> = [1,2,3]
+
+let dizi:[number,string] = [2,'Ankara']
+
+enum Color{Red,Black,Blue}
+let color : Color = Color.Red
+
+let notSure : any = 'Ankara'
+
+//undefined / null
+
+let yas:number
+
+yas = 10
+
+class Customer{
+
+}
